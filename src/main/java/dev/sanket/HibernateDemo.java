192510fc06f343b17ba4782c5aa5fbc834b8d756
sanketmeghani <sanket.meghani@gmail.com>
2016-10-27 12:53:12 +0530

    Using latest libraries

--- src/main/java/dev/sanket/HibernateDemo.java
+++ src/main/java/dev/sanket/HibernateDemo.java
@@ -2,32 +2,24 @@ package dev.sanket;
 
 import dev.sanket.db.DBLayer;
 import dev.sanket.db.model.Address;
-import dev.sanket.db.model.User;
 import dev.sanket.db.model.Vehicle;
 
-public class HibernateDemo
-{
-    public static void main(String args[])
-    {
+public class HibernateDemo {
+    public static void main(String args[]) {
         DBLayer.initialize();
 
         Address address = new Address();
-        
+
         address.setCity("Ahmedabad");
         address.setPincode(380015);
         address.setState("Gujarat");
-        
+
         DBLayer.saveOrUpdate(address);
 
         Vehicle vehicle = new Vehicle();
-        
+
         vehicle.setManufacturer("Honda");
-        
+
         DBLayer.saveOrUpdate(vehicle);
-        
-        User user = new User("Sanket");
-        user.getAddressList().add(address);
-        
-        DBLayer.saveOrUpdate(user);
     }
 }
